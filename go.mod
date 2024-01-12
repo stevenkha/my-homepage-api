b2d5f7a2a53e540dd7be69a494cbd4e0872beaea
stevenkha <stevenkha41@gmail.com>
2024-01-12 13:15:37 -0800

    added environment variable file

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.21.5
 require (
 	github.com/gin-contrib/cors v1.5.0
 	github.com/gin-gonic/gin v1.9.1
+	github.com/joho/godotenv v1.5.1
 )
 
 require (
--- routes/LoginHandler.go
+++ routes/LoginHandler.go
@@ -1,7 +1,16 @@
 package routes
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
 
 func LoginHandler(c *gin.Context) {
-
+	err := godotenv.Load(".env")
+	if err != nil {
+		fmt.Println("Error loading .env file")
+		return
+	}
 }
