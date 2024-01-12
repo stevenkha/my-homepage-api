f2ba2da0ef03228bc0be338507b4e5da617d840d
stevenkha <stevenkha41@gmail.com>
2024-01-12 12:59:43 -0800

    added routes and login handler

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"log"
 
+	"my-homepage-api/routes"
+
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
@@ -11,6 +13,8 @@ func main() {
 	r := gin.Default()
 
 	r.Use(cors.Default())
+
+	r.GET("/api/login", routes.LoginHandler)
 
 	err := r.Run(":8000")
 	if err != nil {
---
+++ routes/LoginHandler.go
@@ -0,0 +1,7 @@
+package routes
+
+import "github.com/gin-gonic/gin"
+
+func LoginHandler(c *gin.Context) {
+
+}
