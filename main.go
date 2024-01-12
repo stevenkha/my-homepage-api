0cd5574751606b78177bb30f881fb7e4ce9cb77a
stevenkha <stevenkha41@gmail.com>
2024-01-12 12:03:53 -0800

    added error handling for running app

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"log"
+
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
@@ -9,4 +11,9 @@ func main() {
 	r := gin.Default()
 
 	r.Use(cors.Default())
+
+	err := r.Run(":8000")
+	if err != nil {
+		log.Fatal(err)
+	}
 }
