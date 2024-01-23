aaf55f7ee1196addaddf9990b1de1844fe879b96
stevenkha <stevenkha41@gmail.com>
2024-01-22 21:47:12 -0800

    updated constants

--- utils/constants.go
+++ utils/constants.go
@@ -1,8 +1,9 @@
 package utils
 
 const (
-	ScheduledAnimeUrl   = "https://aniwatch.to/ajax/schedule/list?tzOffset=480&date="
-	BookmarkedAnimesUrl = "https://yugenanime.tv/mylist/"
-	AnimeListClass      = "list-entries"
-	MangaUrl            = "https://user.mngusr.com/bookmark_get_list_full"
+	ScheduledAnimeUrl       = "https://yugenanime.tv"
+	BookmarkedAnimesUrl     = "https://yugenanime.tv/mylist/"
+	AnimeListClass          = "list-entries"
+	ScheduledAnimeListClass = "schedule-group"
+	MangaUrl                = "https://user.mngusr.com/bookmark_get_list_full"
 )
