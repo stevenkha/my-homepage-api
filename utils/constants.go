d5ccd785b25f96b235ef80ad94b564bb73973a46
stevenkha <stevenkha41@gmail.com>
2024-01-22 20:40:04 -0800

    reverted previous commit

--- utils/constants.go
+++ utils/constants.go
@@ -1,7 +1,7 @@
 package utils
 
 const (
-	AnimeUrl       = "https://aniwatch.to/ajax/schedule/list?tzOffset=480&list=15&date="
+	AnimeUrl       = "https://aniwatch.to/ajax/schedule/list?tzOffset=480&date="
 	AnimeListClass = "list-entries"
 
 	MangaUrl = "https://user.mngusr.com/bookmark_get_list_full"
