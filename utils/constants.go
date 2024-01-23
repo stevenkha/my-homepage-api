2a184787389032addf8ae058cb819675ac42dd9a
stevenkha <stevenkha41@gmail.com>
2024-01-22 20:43:00 -0800

    renamed variable a bit

--- utils/constants.go
+++ utils/constants.go
@@ -1,8 +1,8 @@
 package utils
 
 const (
-	ScheduledAnimeUrl = "https://aniwatch.to/ajax/schedule/list?tzOffset=480&date="
-	BookmarkedAnimes  = "https://yugenanime.tv/mylist/"
+	ScheduledAnimeUrl   = "https://aniwatch.to/ajax/schedule/list?tzOffset=480&date="
+	BookmarkedAnimesUrl = "https://yugenanime.tv/mylist/"
 
 	MangaUrl = "https://user.mngusr.com/bookmark_get_list_full"
 )
